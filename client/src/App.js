859c569ff1a546a90935608cfe42ed031e8a13f2
David White <dwhite0627@gmail.com>
2020-04-06 15:20:15 -0400

    Add Posts component to display all posts from the backend API

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+// Component imports
+import Posts from './components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
